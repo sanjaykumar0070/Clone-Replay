1ad296dabd30b8f8add60ffa94975410429dcdbc
Sanjay Kumar Pogula <sanjaykumarpogula@gmail.com>
2024-05-22 16:22:10 +0530

    Error at seting the backend, working on frontend

---
+++ BackEnd/Replay/src/main/java/com/Replay/ReplayApplication.java
@@ -0,0 +1,13 @@
+package com.Replay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplayApplication.class, args);
+	}
+
+}
---
+++ BackEnd/Replay/src/test/java/com/Replay/ReplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Replay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
